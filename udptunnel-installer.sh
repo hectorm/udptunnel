13578016f47b2f02e85c5ab4aa92e820e1f25f79
Héctor Molinero Fernández <hector@molinero.dev>
2021-01-22 19:24:03 +0100

    Updated installer

--- udptunnel-installer.sh
+++ udptunnel-installer.sh
@@ -2,14 +2,14 @@
 
 set -eu
 
-SRCDIR=${SRCDIR:-$(dirname "$(readlink -f "$0")")}
-cd "${SRCDIR:?}"
+SRCDIR=${SRCDIR:-$(CDPATH='' cd -- "$(dirname -- "${0:?}")" && pwd -P)}
+cd -- "${SRCDIR:?}"
 
-export CFLAGS="${CFLAGS-} -O2 -fPIC -fPIE -fstack-protector-strong -frandom-seed=42 -Wformat -Werror=format-security"
+export CFLAGS='-O2 -fPIC -fPIE -fstack-protector-strong -frandom-seed=42 -Wformat -Werror=format-security'
 if [ "$(uname -m)" = 'x86_64' ]; then export CFLAGS="${CFLAGS-} -fstack-clash-protection -fcf-protection=full"; fi
-export CPPFLAGS="${CPPFLAGS-} -Wdate-time -D_FORTIFY_SOURCE=2"
-export LDFLAGS="${LDFLAGS-} -Wl,-pie -Wl,-z,defs -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack"
-export SOURCE_DATE_EPOCH=1
+export CPPFLAGS='-Wdate-time -D_FORTIFY_SOURCE=2'
+export LDFLAGS='-Wl,-pie -Wl,-z,defs -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack'
+export LC_ALL=C TZ=UTC SOURCE_DATE_EPOCH=1
 
 make clean
 make -j"$(nproc)"
